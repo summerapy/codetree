00627d6e8e42601a42217a3565386340268badff
summer <167855067+summerapy@users.noreply.github.com>
2025-03-26 15:17:35 +0900

    Title: 사칙연산 연습, Time: 89ms, Memory: 9MB, Status: Passed - CodeTree

---
+++ 250326/사칙연산 연습/practice-basic-arithmetic-operations.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 30;
+        int c = a+b;
+        int x = c/8;
+        int y = c%8;
+        System.out.println(x);
+        System.out.println(y);
+    }
+}
