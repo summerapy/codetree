ffaa452c918a5625c3f832bac177561820a90d0c
summer <167855067+summerapy@users.noreply.github.com>
2025-04-09 15:15:00 +0900

    Title: 달에서 무게 구하기, Time: 104ms, Memory: 9MB, Status: Passed - Codetree

---
+++ 250409/달에서 무게 구하기/weight-on-the-moon.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        double a = 13;
+        double b = 0.165;
+        double c = a*b;
+        System.out.printf("%.0f * %.6f = %.6f\n", a, b, c);
+
+    }
+}
