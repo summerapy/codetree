b42466cd9f3ef15ec20be3a1fb4622860152858a
summer <167855067+summerapy@users.noreply.github.com>
2025-05-21 14:58:23 +0900

    Title: 세 실수의 반올림, Time: 160ms, Memory: 15MB, Status: Passed - Codetree

---
+++ 250521/세 실수의 반올림/rounding-of-three-actual-numbers.js
@@ -0,0 +1,12 @@
+// 변수 선언, 입력
+const fs = require("fs");
+let input = fs.readFileSync(0).toString().split("\n");
+
+let a = Number(input[0]);
+let b = Number(input[1]);
+let c = Number(input[2]);
+
+// 출력
+console.log(a.toFixed(3))
+console.log(b.toFixed(3))
+console.log(c.toFixed(3));
